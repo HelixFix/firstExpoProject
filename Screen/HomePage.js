0fe6784d50236814c518114975010694f67c03b0
HelixFix <jerome.rinner@gmail.com>
2021-05-17 15:42:44 +0200

    Add button

--- Screen/HomePage.js
+++ Screen/HomePage.js
@@ -1,23 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up !</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  return (
+    <View style={styles.container}>
+      <Text>Open up !</Text>
+      <StatusBar style="auto" />
+      <Button
+        //onPress={onPressLearnMore}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
