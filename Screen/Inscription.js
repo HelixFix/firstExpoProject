4247358164731d03bd4bddfe74a347b057aa4144
HelixFix <jerome.rinner@gmail.com>
2021-05-31 16:06:55 +0200

    Call validator functions

--- Screen/Inscription.js
+++ Screen/Inscription.js
@@ -1,12 +1,48 @@
 import React from "react";
 import Title from "../Components/Title";
-import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Text, Alert } from "react-native";
 import Button from "../Components/Button";
 import EmailInput from "../Components/EmailInput";
 import PasswordInput from "../Components/PasswordInput";
 import TexteInput from "../Components/TexteInput";
+import {
+  emailValidator,
+  nameValidator,
+  passwordValidator,
+} from "../core/utils";
 
 export default class Inscription extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  alerte() {
+    Alert.alert("Erreur", "Veuillez remplir correctement les champs", [
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+  }
+
+  onSignUpPressed() {
+    console.log("click");
+    console.log(this.props);
+    const nameError = nameValidator(this.state.name);
+    const emailError = emailValidator(this.state.email);
+    const passwordError = passwordValidator(this.state.password);
+
+    if (nameError || emailError || passwordError) {
+      this.alerte();
+      return;
+    } else {
+      // const action
+    }
+  }
+
   render() {
     const { navigate } = this.props.navigation;
 
@@ -19,13 +55,18 @@ export default class Inscription extends React.Component {
           autoCompleteType = "name"
           textContentType  = "name"
           autoCompleteType = "name"
+          value            = {this.state.name}
         />
 
         <EmailInput />
 
         <PasswordInput />
 
-        <Button color = "#841584" title = "Inscription" />
+        <Button
+          color   = "#841584"
+          title   = "Inscription"
+          onPress = {() => this.onSignUpPressed()}
+        />
 
         <Text>
           Déjà inscrit ?
