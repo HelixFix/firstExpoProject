7b33b59f8bebf910f3c18abb19189dbba43d9f2f
HelixFix <jerome.rinner@gmail.com>
2021-06-21 15:30:30 +0200

    Remove reducer

--- App.js
+++ App.js
@@ -8,25 +8,23 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Provider } from "react-redux";
-import Store from "./Store/ConfigStore";
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <Provider store={Store}>
-      <NavigationContainer>
-        <Stack.Navigator>
+    <NavigationContainer>
+    <Stack.Navigator>
 
-          <Stack.Screen name = "HomePage" component       = {HomePage} />
-          <Stack.Screen name = "LoginScreen" component    = {Connexion} />
-          <Stack.Screen name = "ForgotPW" component       = {MotDePasseOublie} />
-          <Stack.Screen name = "UserHomePage" component   = {AccueilUtilisateur} />
-          <Stack.Screen name = "Registerscreen" component = {Inscription} />
+      <Stack.Screen name = "HomePage" component       = {HomePage} />
+      <Stack.Screen name = "LoginScreen" component    = {Connexion} />
+      <Stack.Screen name = "ForgotPW" component       = {MotDePasseOublie} />
+      <Stack.Screen name = "UserHomePage" component   = {AccueilUtilisateur} />
+      <Stack.Screen name = "Registerscreen" component = {Inscription} />
 
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 };
 
